a8ce8910ab5d933352aaf27fe27d7434f55134e0
KRISHNA-DHARSANDIA <kp064342@gmail.com>
2025-01-13 20:32:27 +0530

    fix:auth URL

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex pt-10 flex-col items-center text-center min-h-screen bg-black">
+    <div className="flex pt-10 flex-col items-center text-center min-h-screen bg-white">
       <SignIn />
     </div>
   );
