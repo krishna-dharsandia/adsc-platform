65500d98033e6efcb179ca0e18fd12264d8a05df
Jenil Desai <jenildev91@gmail.com>
2025-01-27 00:13:01 +0530

    feat: Update social media links and replace icons for Instagram and LinkedIn

--- components/layout/NavIcons.tsx
+++ components/layout/NavIcons.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 const NavIcons = () => {
   return (
     <div className="flex items-center gap-4 md:gap-4">
-      <Link href="https://github.com/adel-gu" className="group flex items-center justify-center gap-x-1.5" target="_blank">
-        <Image src="/github.svg" alt="Link to Adel's gitHub profile" width={25} height={25} />
-        <span className="hidden text-xs text-white opacity-50 transition group-hover:opacity-100 md:inline">Github</span>
+      <Link href="https://www.instagram.com/adsc.atmiya" className="group flex items-center justify-center gap-x-1.5" target="_blank">
+        <Image src="/icons/instagram.svg" alt="Link to ADSC's gitHub profile" width={25} height={25} />
+        <span className="hidden text-xs text-white opacity-50 transition group-hover:opacity-100 md:inline">Instagram</span>
       </Link>
-      <Link href="https://www.linkedin.com/in/adelguitoun/" className="group flex items-center justify-center gap-x-1.5" target="_blank">
-        <Image src="/linkedin.svg" alt="Link to Adel's linkedin profile" width={28} height={28} />
+      <Link href="https://www.linkedin.com/company/adsc-atmiya-university" className="group flex items-center justify-center gap-x-1.5" target="_blank">
+        <Image src="/icons/linkedin.svg" alt="Link to ADSC's linkedin profile" width={28} height={28} />
         <span className="hidden text-xs text-white opacity-50 transition group-hover:opacity-100 md:inline">LinkedIn</span>
       </Link>
     </div>
