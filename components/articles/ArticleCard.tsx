88a7255e4fa361bde80f295466f07f261705d4c2
Jenil Desai <jenildev91@gmail.com>
2025-02-01 09:01:08 +0530

    feat: Update ArticleCard and ProjectCard components with new content and layout adjustments

--- components/articles/ArticleCard.tsx
+++ components/articles/ArticleCard.tsx
@@ -1,34 +1,15 @@
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import Link from 'next/link';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 const ArticleCard = () => {
   return (
-    <Link
-      href="https://medium.com/@adeelguitoun/a-quick-guide-to-ruby-on-rails-migrations-with-examples-8b3da8077ed7"
-      target="_blank"
-    >
+    <Link href="https://gdsc-atmiyauniversity.vercel.app/TechTales.pdf" target="_blank">
       <Card className="max-w-[1200px] border-none bg-background shadow-2xl transition-all hover:-translate-y-1.5 hover:bg-[#1a1a1a4d]">
         <CardHeader className="space-y-3">
-          <CardTitle className="text-white">
-            A Quick Guide to Ruby on Rails Migrations with examples!!
-          </CardTitle>
-          <CardDescription className="text-silverchalice">
-            How to set up a Rails app with PostgreSQL, create and configure
-            migrations to define and modify database tables, and manage
-            relationships between them. The tutorial emphasizes Rails
-            conventions, automatic features, and how to handle schema changes
-            effectively.
-          </CardDescription>
+          <CardTitle className="text-white">Tech Tales</CardTitle>
+          <CardDescription className="text-silverchalice">Explore the dynamic world of our club in 'Innovate & Elevate.' From AI-hosted events and programming fundamentals to data insights and hackathon chronicles, journey through our past works and the Google Solution Challenge. Introducing 'Innovate & Elevate,' this magazine offers a sneak peek into the vibrant events and collaborative innovation at GDSC.</CardDescription>
         </CardHeader>
-        <CardFooter className="text-gray-400">
-          6 min read · Jan 24, 2023
-        </CardFooter>
+        <CardFooter className="text-gray-400">6 min read · Nov 01, 2023</CardFooter>
       </Card>
     </Link>
   );
--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,15 +1,9 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const ProjectCard = ({ src, title }: { src: string; title: string }) => {
   return (
-    <div className="z-10 h-[400px] w-[350px] overflow-hidden rounded-xl shadow-2xl md:mt-32">
-      <Image
-        src={src}
-        alt={title}
-        width={350}
-        height={400}
-        className="size-full bg-cover bg-center"
-      />
+    <div className="z-10 h-[400px] w-[500px] overflow-hidden rounded-xl shadow-2xl md:mt-32">
+      <Image src={src} alt={title} width={500} height={400} className="size-full bg-cover bg-center" />
     </div>
   );
 };
