6a5dd85cdca413e64ae1ab8225c3abbed3c08053
Jenil Desai <jenildev91@gmail.com>
2025-02-04 00:09:15 +0530

    fix: Update ProjectCard component for responsive image layout

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const ProjectCard = ({ src, title }: { src: string; title: string }) => {
   return (
-    <div className="z-10 h-[400px] w-[500px] overflow-hidden rounded-xl shadow-2xl md:mt-32">
-      <Image src={src} alt={title} width={500} height={400} className="size-full bg-cover bg-center" />
+    <div className="z-10 h-auto w-full max-w-[500px] overflow-hidden rounded-xl shadow-2xl md:mt-32">
+      <Image src={src} alt={title} layout="responsive" width={500} height={400} className="size-full bg-cover bg-center" />
     </div>
   );
 };
